204519114fbcb864e34cf8595f5f53b536e9d2e2
Dung Viet <vietdung299@gmail.com>
2022-12-16 11:50:03 +0700

    update lint

--- src/tests/do.test.js
+++ src/tests/do.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../App";
 import { QueryClient, QueryClientProvider } from "react-query";
-import '@testing-library/jest-dom'
+import "@testing-library/jest-dom";
 
 test("render react link", () => {
   const client = new QueryClient();
@@ -12,6 +12,6 @@ test("render react link", () => {
       <App />
     </QueryClientProvider>
   );
-  const h1Element = screen.getByText('Star Wars Info - Re-deployment');
+  const h1Element = screen.getByText("Star Wars Info - Re-deployment");
   expect(h1Element).toBeInTheDocument();
 });
